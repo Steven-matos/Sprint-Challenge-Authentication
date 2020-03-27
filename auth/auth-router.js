5c0dfd23ea8258749fbf9b4527f4c92062af20cf
Steven <38477361+Steven-matos@users.noreply.github.com>
2020-03-27 11:27:41 -0400

    created middleware and auth router

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,64 @@
-const router = require('express').Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const router = require("express").Router();
 
-router.post('/register', (req, res) => {
-  // implement registration
+const { jwtSecret } = require("../config/secret");
+const Users = require("../user/user-model");
+
+router.post("/register", (req, res) => {
+  const userInfo = req.body;
+
+  const hash = bcrypt.hashSync(userInfo.password, 8);
+
+  userInfo.password = hash;
+
+  Users.add(userInfo)
+    .then(user => {
+      res.status(201).json(user);
+    })
+    .then(err => {
+      console.log(err);
+      res.status(500).json({
+        message: "failed to register user",
+        error: err
+      });
+    });
 });
 
-router.post('/login', (req, res) => {
-  // implement login
+router.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  Users.getBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+        res.status(200).json({
+          Welcome: user.password,
+          token
+        });
+      } else {
+        res.status(401).json({ message: "Invalid Credentials" });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        message: "Error finding user",
+        error: err
+      });
+    });
 });
+
+function generateToken(user) {
+  const payload = {
+    username: user.username
+  };
+
+  const options = {
+    expiresIn: "1h"
+  };
+  return jwt.sign(payload, jwtSecret, options);
+}
 
 module.exports = router;
